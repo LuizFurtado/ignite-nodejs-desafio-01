0bb8c2e6611fb588a4255b989ca885eb36762925
Luiz Furtado <luizfpfurtado@hotmail.com>
2021-03-17 07:48:58 -0300

    Comentado array de usuários de teste

--- src/index.js
+++ src/index.js
@@ -8,32 +8,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  {
-    id: "f03b8c0a-9c95-4933-9ad1-2ebc800a8682",
-    name: "John Doe",
-    username: "jdoe",
-    todos: [{
-      id: "ebcc4b5b-24c1-4471-9ec3-46c5bd6100470",
-      title: "My one todo",
-      dealine: "2021-03-19T00:00:00.000Z",
-      created_at: "2021-03-16T00:00:00.000Z",
-      done: false
-    }]
-  },
-  {
-    id: "33a97198-8ca5-4660-bb91-505cfec933ee",
-    name: "Jane Doe",
-    username: "thejane",
-    todos: [{
-      id: "bb21eae7-e82e-426c-9912-c0383f9a53c7",
-      title: "Either another todo",
-      dealine: "2021-03-19T00:00:00.000Z",
-      created_at: "2021-03-16T00:00:00.000Z",
-      done: false
-    }]
-  }
-];
+// const users = [
+//   {
+//     id: "f03b8c0a-9c95-4933-9ad1-2ebc800a8682",
+//     name: "John Doe",
+//     username: "jdoe",
+//     todos: [{
+//       id: "ebcc4b5b-24c1-4471-9ec3-46c5bd6100470",
+//       title: "My one todo",
+//       dealine: "2021-03-19T00:00:00.000Z",
+//       created_at: "2021-03-16T00:00:00.000Z",
+//       done: false
+//     }]
+//   },
+//   {
+//     id: "33a97198-8ca5-4660-bb91-505cfec933ee",
+//     name: "Jane Doe",
+//     username: "thejane",
+//     todos: [{
+//       id: "bb21eae7-e82e-426c-9912-c0383f9a53c7",
+//       title: "Either another todo",
+//       dealine: "2021-03-19T00:00:00.000Z",
+//       created_at: "2021-03-16T00:00:00.000Z",
+//       done: false
+//     }]
+//   }
+// ];
+
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
